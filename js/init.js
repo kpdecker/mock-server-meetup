d3ec0ac826a4d411a6d7b61d456844d04aec645f
kpdecker <kpdecker@gmail.com>
2012-08-20 01:08:46 -0500

    Pull in thorax lib

---
+++ js/init.js
@@ -0,0 +1,11 @@
+Thorax.templatePathPrefix = 'templates/';
+
+var Application = window.Application = module.exports = new Thorax.Application(module.exports);
+$(document).ready(function() {
+  $('body').append(Application.el);
+
+  Application.start({
+    pushState: false,
+    root: '/'
+  });
+});
